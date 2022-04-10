b71a2f46453eecec84d8683aeb979a52013d0193
RizFurrs <103049737+RizFurrs@users.noreply.github.com>
2022-04-10 12:36:45 +0800

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -3,11 +3,11 @@ __path = process.cwd()
 var express = require('express');
 var router = express.Router();
 
-router.get('/', (req, res) => {
+/*router.get('/', (req, res) => {
     res.sendFile(__path + '/docs/home.html')
-})
+})*/
 
-router.get('/docs', (req, res) => {
+router.get('/', (req, res) => {
 	res.sendFile(__path + '/docs/dashboard.html')
 })
 router.get('/docs/asupan', (req, res) => {
